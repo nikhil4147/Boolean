f1256c3cd073315508df508683035ec4da69b729
Nikhil4147 <nikhil.waghmare4147@gmail.com>
2024-01-28 16:27:41 +0530

    commit pass message here

---
+++ Boolean.py
@@ -0,0 +1,18 @@
+#Boolean Values
+#In programming you often need to know if an expression is True or False.
+
+print(10 < 8)
+print(62 > 5)
+print(65 == 52)
+
+
+#Example 1:
+
+a = 42
+b = 563
+
+if b > a:
+    print("b is greater than a")
+
+else:
+    print("a is greater than b")
